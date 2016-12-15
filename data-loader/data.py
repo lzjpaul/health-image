d966fd300f25594061935654843431502df3101f
“zhaojing” <zhaojing@comp.nus.edu.sg>
2016-12-15 11:18:48 +0800

    enhance brightness

--- data-loader/data.py
+++ data-loader/data.py
@@ -1,3 +1,4 @@
+from singa import image_tool
 import dicom
 
 def load_from_dicom(img_path):
@@ -15,3 +16,19 @@ def load_from_csv(img_path):
 def get_mean(mean_path):
     mean = np.genfromtxt(mean_path, delimiter = ',')
     return mean
+
+def load_from_img_enhance(img_path, num):
+    new_imgs = []
+    img_path = img_path.replace('dcm', 'jpeg')
+    img = image_tool.load_img(img_path, grayscale=True)
+    for i in range(num):
+        new_imgs.append(image_tool.enhance(img, scale = 0.2))
+    return new_imgs
+
+def load_from_png_enhance(img_path, num):
+    new_imgs = []
+    img_path = img_path.replace('dcm', 'png')
+    img = image_tool.load_img(img_path, grayscale=True)
+    for i in range(num):
+        new_imgs.append(image_tool.enhance(img, scale = 0.2))
+    return new_imgs
